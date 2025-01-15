2f96ec469d69350b24340f9be62b75018c6c6542
Mahfuzur Rahman Emon <43314141+emon-swe-sust@users.noreply.github.com>
2025-01-15 19:43:13 +0600

    html + js

---
+++ html+js/js-1.js
@@ -0,0 +1,31 @@
+a = 5
+str = "The value of a"
+console.log(str,a)
+
+x = 5
+y = 7
+
+p = x+y
+console.log(p)
+q = x-y
+r = x*y
+s = x / y
+p = x**y
+t = y%x
+
+console.log(p, q, r, s,t)
+
+a = 5
+a++
+console.log("Line 20", a)
+a--
+console.log("Line 22",a)
+a = a+10
+console.log("Line 24",a)
+
+town = "Dhaka"
+country = "Bangladesh"
+addr = town + "," + country
+console.log(addr)
+n=null
+console.log(n)
---
+++ html+js/js-2.js
@@ -0,0 +1,17 @@
+function def() {
+    a = 6
+    console.log(a)
+}
+
+def()
+
+function sum(a,b) {
+    x = a + b
+    console.log(x)
+    return a*b
+}
+sum(6,7)
+sum(10,20)
+sum("My Name is ", "Emon")
+pqr = sum(8,9)
+console.log("pqr: ", pqr)
